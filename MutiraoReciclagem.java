f806786c9694543cbd91bd9441fef1d23a021077
Carlos Arthur <betoc8793@gmail.com>
2026-09-02 12:48:04 -0300

    Add MutiraoReciclagem class for recycling events

---
+++ MutiraoReciclagem.java
@@ -0,0 +1,29 @@
+package GestãodeAções;
+
+import java.time.LocalDateTime;
+
+public class MutiraoReciclagem extends Acao{
+    private int duracaoEmHoras;
+
+    public MutiraoReciclagem(int maxCapacidade, LocalDateTime data, String descricao, String titulo,int duracaoEmHoras) {
+
+        super(maxCapacidade, data, descricao, titulo);
+
+        this.duracaoEmHoras = duracaoEmHoras;
+
+
+
+    }
+
+    public int getDuracaoEmHoras() {
+        return duracaoEmHoras;
+    }
+
+    public void setDuracaoEmHoras(int duracaoEmHoras) {
+        this.duracaoEmHoras = duracaoEmHoras;
+    }
+
+    public int calcularPontuacao(){
+        return 4 * duracaoEmHoras;
+    }
+}
